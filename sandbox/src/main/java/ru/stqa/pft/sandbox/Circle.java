6e02f9614d5c1e58705be92fd77efc795a8dd6a4
DivineHelp <kriptozoj@gmail.com>
2021-02-28 20:33:29 +0300

    Создание функции подсчета площади круга и перевод фурнкции для квадрата в метод.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Circle.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class Circle {
+    public double r;
+    public Circle(double r) {
+        this.r = r;
+    }
+
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,22 +7,27 @@ public class MyFirstProgram {
 
 		Square s = new Square(5);
 //		s.l = 5;
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle();
 		r.a = 3;
 		r.b = 4;
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+
+		Circle c = new Circle(10);
+		System.out.println("Площадь круга с радиусом " + c.r + " = " + area(c));
 	}
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
 //Функция подсчета площади квадрата
-	public static double area(Square s) {
-		return s.l * s.l;
-	}
+
 
 	public static double area(Rectangle r) {
 		return r.a * r.b;
+	}
+
+	public static double area(Circle c) {
+		return c.r * c.r * 3.14;
 	}
 }
